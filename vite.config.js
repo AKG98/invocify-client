0d5af91e145b62a4fd83730652f1ef4206022b41
Avinash Gupta <93136086+AKG98@users.noreply.github.com>
2024-10-10 09:04:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://invocify-server.onrender.com',
-        secure: false,
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://invocify-server.onrender.com',
+  //       secure: false,
+  //     }
+  //   }
+  // }
 })
